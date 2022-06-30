9cd3591dfba4ae4b2959ff2c0d96fa1098a4d729
MD. TANIM HOSSAIN <53136185+ccttanim@users.noreply.github.com>
2022-06-30 09:57:30 +0600

    Create Pow(x, n).cpp

---
+++ week08/Pow(x, n).cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+       double modularExpo(double x, long long n)
+        {
+            if(n == 0) return 1.0;
+            if(n % 2 == 0)
+            {
+              auto temp = modularExpo(x, n / 2);
+              return (temp * temp);
+            }
+           return x * modularExpo(x, n-1);
+        }
+    double myPow(double x, int n) {
+     bool invflag = n < 0;
+        if(n == 0) return 1.0;
+        long long bn = n < 0 ? 1LL * (-1) * n : n;
+        double res = modularExpo(x, bn);
+        res = invflag ? 1.0 / res : res;
+        return res;
+    }
+};
